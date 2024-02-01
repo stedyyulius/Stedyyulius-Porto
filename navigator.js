a13462872b0c403708356e751f1d0c22d55974fd
stedy <stedyyulius@gmail.com>
2024-02-01 17:32:53 +0400

    add a movable fat black cat

--- navigator.js
+++ navigator.js
@@ -1,25 +1,55 @@
+import * as spline from "https://esm.run/@splinetool/runtime";
+
+const mapCanvas = document.getElementById("map");
+
+mapCanvas.width = window.innerWidth;
+mapCanvas.height = window.innerHeight;
+
+const map = new spline.Application(mapCanvas);
+
+await map.load("https://prod.spline.design/EaimB9ms4DH6wWiU/scene.splinecode");
+
 const pressButton = (direction) => {
     const element = document.getElementsByClassName(direction)[0];
-    element.classList.add("pressed");
-    setTimeout(() => {
-        element.classList.remove("pressed");
-    }, 200);
+    if (element) {
+        element.classList.add("pressed");
+        setTimeout(() => {
+            element.classList.remove("pressed");
+        }, 200);
+    }
+
 };
+
+const movePixel = 50;
+
+const directions = {
+    UP: "ArrowUp",
+    DOWN: "ArrowDown",
+    RIGHT: "ArrowRight",
+    LEFT: "ArrowLeft"
+}
+
+const fatCat = map.findObjectByName('Cat');
 
 document.onkeydown = (e) => {
-    e = e || window.event;
-    if (e.keyCode === 38) {
-        pressButton("up");
-    } else if (e.keyCode === 40) {
-        pressButton("bottom");
-    } else if (e.keyCode === 37) {
-        pressButton("left");
-    } else if (e.keyCode === 39) {
-        pressButton("right");
-    }
-};
 
-const map = document.getElementById("map");
+    pressButton(e.key);
+
+    if (e.key === "ArrowUp") {
+        fatCat.position.x += movePixel;
+        fatCat.position.z -= (movePixel * 2 / 5);
+    } else if (e.key === "ArrowDown") {
+        fatCat.position.x -= movePixel;
+        fatCat.position.z += (movePixel * 2 / 5);
+    } else if (e.key === "ArrowLeft") {
+        fatCat.position.z -= movePixel;
+        fatCat.position.x -= (movePixel * 2 / 5);
+    } else if (e.key === "ArrowRight") {
+        fatCat.position.z += movePixel;
+        fatCat.position.x += (movePixel * 2 / 5);
+    }
+
+};
 
 map.onwheel = (event) => {
     event.preventDefault();
